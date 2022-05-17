6c4d6c2260a0b3e768e56daf5b5c7c4e98f20453
Md. Mostak Shahid <mostak.shahid@gmail.com>
2022-05-18 01:42:00 +0600

    Update Search.js

--- src/Page/Search/Search.js
+++ src/Page/Search/Search.js
@@ -1,6 +1,18 @@
-import React from 'react'
-
-const Search = () => {
+import React, { useEffect, useState } from 'react';
+import { useLocation, useParams } from "react-router-dom";
+import Config from "../../Config.json";
+const Search = (props) => {
+  
+  const location = useLocation();
+  const params = useParams();
+  const [searchPageData,setBlogPageData]=useState([]);
+  const [postsData,setPostsData]=useState([]);
+  const [loading,setLoading]=useState(true);
+  useEffect(()=>{
+      const url = Config.API_BASE + "data-single/" + params.slug;//api url
+      fetch(url).then(resp=>resp.json())//calling url by method GET
+      .then(resp=>searchPageData(resp))//setting response to state posts
+  },[]);
   return (
     <div>Search</div>
   )
